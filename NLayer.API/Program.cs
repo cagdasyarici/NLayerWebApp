cc23832c4ce0b6ce2c2b035de78d81b0a441ccc0
cagdasyarici <cagdasyarici16@gmail.com>
2023-07-12 16:27:34 +0300

    Validate up

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -19,6 +19,7 @@ namespace NLayer.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
             
             builder.Services.AddControllers();
             builder.Services.AddDbContext<AppDbContext>(x =>
